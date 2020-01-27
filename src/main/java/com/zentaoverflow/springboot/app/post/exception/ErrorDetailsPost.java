f50d85d8438f85f11f6409c2594ae4ee4fcc4dcb
Javier Fonseca <javier.fonseca@zentagroup.com>
2020-01-27 16:38:09 -0300

    Clases de Error finalizadas

---
+++ src/main/java/com/zentaoverflow/springboot/app/post/exception/ErrorDetailsPost.java
@@ -0,0 +1,29 @@
+package com.zentaoverflow.springboot.app.post.exception;
+import java.util.Date;
+
+public class ErrorDetailsPost {
+
+	    private Date timestamp;
+	    private String message;
+	    private String details;
+	    
+	    public ErrorDetailsPost(Date timestamp, String message, String details) {
+	        super();
+	        this.timestamp = timestamp;
+	        this.message = message;
+	        this.details = details;
+	   }
+	    
+	    public Date getTimestamp() {
+	        return timestamp;
+	   }
+
+	   public String getMessage() {
+	        return message;
+	   }
+
+	   public String getDetails() {
+	        return details;
+	   }
+	
+}
